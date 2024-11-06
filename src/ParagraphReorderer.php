7fe9ac98594d526bdcd0f71c4fc06c6509c38eb5
Yisrael Dov Lebow <lebow@lebowtech.com>
2024-11-07 01:22:42 +0200

    Refactor sentence splitting into dedicated method
    Restructured the `ParagraphReorderer` class by extracting the logic
    for splitting sentences from the `reorder` method into a new private
    method `splitSentences`. This improves code organization and
    reusability. The `sortByLength` method remains unchanged but is now
    called after the sentence split, enhancing readability and
    maintainability.

--- src/ParagraphReorderer.php
+++ src/ParagraphReorderer.php
@@ -4,23 +4,29 @@ namespace Bhbsd;
 
 class ParagraphReorderer
 {
+    public function reorder(string $inputParagraph): string
+    {
+        $sentences = $this->splitSentences($inputParagraph);
+        $this->sortByLength($sentences);
+        return array_reduce($sentences, function ($acc, $sen) {
+            return $acc . $sen;
+        },
+            "");
+    }
+
+    private function splitSentences(string $inputParagraph): array
+    {
+        preg_match_all('/([^.?!]+[.?!]?)/', $inputParagraph, $matches);
+        $sentences = $matches[0];
+        return $sentences;
+    }
+
+
     /**
      * @param array<string> $sentences
      */
     private function sortByLength(array &$sentences): void
     {
         usort($sentences, fn($a, $b)=> (strlen($a) < strlen($b)) ? -1 : 1);
-    }
-
-    public function reorder(string $inputParagraph): string
-    {
-        preg_match_all('/([^.?!]+[.?!]?)/', $inputParagraph, $matches);
-        $sentences = $matches[0];
-
-        $this->sortByLength($sentences);
-        return array_reduce($sentences, function ($acc, $sen) {
-            return $acc . $sen;
-        },
-            "");
     }
 }
