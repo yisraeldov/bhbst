95940da9c275dc2e5d5c933febeadc7dd174281a
Yisrael Dov Lebow <lebow@lebowtech.com>
2024-11-07 01:30:45 +0200

    Reorder sentences by length in ParagraphReorderer
    Refactored the `reorder` method to directly return the sorted
    sentences using `implode` after calling `sortByLength`. Changed
    `sortByLength` to return the sorted array instead of modifying it in
    place and updated its type annotations. Added a detailed docblock for
    `reorder` to describe its functionality.

--- src/ParagraphReorderer.php
+++ src/ParagraphReorderer.php
@@ -4,13 +4,19 @@ namespace Bhbsd;
 
 class ParagraphReorderer
 {
+    /**
+     * accepts a paragraph and reorders all sentences according to
+     * their lengths (characters) in ascending order.
+     */
     public function reorder(string $inputParagraph): string
     {
         $sentences = $this->splitSentences($inputParagraph);
-        $this->sortByLength($sentences);
-        return implode('', $sentences);
+        return implode('', $this->sortByLength($sentences));
     }
 
+    /**
+     * @return array<string>
+     */
     private function splitSentences(string $inputParagraph): array
     {
         preg_match_all('/([^.?!]+[.?!]?)/', $inputParagraph, $matches);
@@ -18,12 +24,13 @@ class ParagraphReorderer
         return $sentences;
     }
 
-
     /**
      * @param array<string> $sentences
+     * @return array<string>
      */
-    private function sortByLength(array &$sentences): void
+    private function sortByLength(array $sentences): array
     {
         usort($sentences, fn($a, $b)=> (strlen($a) < strlen($b)) ? -1 : 1);
+        return $sentences;
     }
 }
