672c09c5548312a5aef952d902521e76f06cca31
Yisrael Dov Lebow <lebow@lebowtech.com>
2024-11-07 01:19:50 +0200

    Refactor: Extract sentence sorting into a separate method
    Abstracted the logic for sorting sentences by their length into a
    private method `sortByLength` within the `ParagraphReorderer` class
    for improved readability and maintainability. Adjusted the main
    `reorder` method to utilize this new private method.

--- src/ParagraphReorderer.php
+++ src/ParagraphReorderer.php
@@ -4,12 +4,20 @@ namespace Bhbsd;
 
 class ParagraphReorderer
 {
+    /**
+     * @param array<string> $sentences
+     */
+    private function sortByLength(array &$sentences): void
+    {
+        usort($sentences, fn($a, $b)=> (strlen($a) < strlen($b)) ? -1 : 1);
+    }
+
     public function reorder(string $inputParagraph): string
     {
         preg_match_all('/([^.?!]+[.?!]?)/', $inputParagraph, $matches);
         $sentences = $matches[0];
 
-        usort($sentences, fn($a, $b)=> (strlen($a) < strlen($b)) ? -1 : 1);
+        $this->sortByLength($sentences);
         return array_reduce($sentences, function ($acc, $sen) {
             return $acc . $sen;
         },
