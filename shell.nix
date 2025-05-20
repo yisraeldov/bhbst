5dd23fd84fbb1bf0ade943111f6d4ce252cb72c2
Yisrael Dov Lebow <lebow@lebowtech.com>
2025-05-20 21:02:45 +0300

    Add Node.js project setup with dependencies and Cypress configuration
    This commit introduces a Node.js project setup with the addition of
    `package.json`, which specifies project metadata and dependencies,
    including `cherry-cljs` and `devDependencies` like `cypress`. A
    `package-lock.json` is generated to lock down specific versions of
    these dependencies for consistent installations. The `.gitignore` file
    is updated to ignore `node_modules`.
    
    Additionally, `shell.nix` is updated to configure Nix development
    environments, now including Node.js and a specific version of
    `cypress` that is fetched from an external URL. The
    `CYPRESS_INSTALL_BINARY` and `CYPRESS_RUN_BINARY` environment
    variables are set to control Cypress binary installations and usage in
    a Unix environment. This setup prepares the project for both Node
    development and testing workflows utilizing Cypress.

--- shell.nix
+++ shell.nix
@@ -1,12 +1,26 @@
 {pkgs ? import <nixpkgs> {}}:
+let
+  cypress-newest = pkgs.cypress.overrideAttrs (oldAttrs: rec {
+    version = "14.2.0";
+    platform = "linux-x64";
+    src = pkgs.fetchurl {
+      url = "https://cdn.cypress.io/desktop/${version}/${platform}/cypress.zip";
+      sha256 = "sha256-uANUePCzOCPoUrMz5KaWVYF+F9LKnFXoV5U8vZvh1K0=";
+    };
+  });
+in
 pkgs.stdenv.mkDerivation{
+  CYPRESS_INSTALL_BINARY = 0;
+  CYPRESS_RUN_BINARY = "${cypress-newest}/bin/Cypress";
   nativeBuildInputs = with pkgs; [
     php
     phpPackages.php-codesniffer
     phpPackages.composer
     phpactor
     onefetch
+    nodejs
     vscode-langservers-extracted
+    cypress-newest
   ];
   name = "bhbsd";
 }
